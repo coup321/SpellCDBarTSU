3a4d17e0e72020cecac467d02cc964a713eee95d
coup321 <coup321@gmail.com>
2024-05-23 08:52:24 -0400

    added entry info and options

--- new_trigger.lua
+++ new_trigger.lua
@@ -2,34 +2,43 @@ function(allstates, ...)
     local event = select(1, ...)
     local subEvent = select(3, ...)
 
-    if event == "FRAME_UPDATE" and aura_env.config.allowCdFromStartOfCombat then
-        local guid, newState = aura_env.handleFrameUpdate()
-        if guid and newState then
-            allstates[guid] = newState
+    if event == "FRAME_UPDATE"  then
+        local newStates = aura_env.handleFrameUpdate()
+        for guid_spellId, state in pairs(newStates) do
+            allstates[guid_spellId] = state 
+        end
+        return true
+    end
+
+    if subEvent == "SPELL_CAST_START"  then
+        local sourceGuid = select(5, ...) 
+        local spellId = select(13, ...)
+        local newState = aura_env.handleSpellCastStart(...)
+        if sourceGuid and newState then
+            allstates[sourceGuid..spellId] = newState
             return true
         end
     end
 
-    if subEvent == "SPELL_CAST_START" and aura_env.config.useSpellCastStart then
-        local sourceGuid = select(5, ...) 
-        local newState = aura_env.handleSpellCastStart(...)
-        allstates[sourceGuid] = newState
-        return true
-    end
-    if event == "UNIT_SPELLCAST_SUCCEEDED" and aura_env.config.useUnitSpellcastSucceeded then
+    if event == "UNIT_SPELLCAST_SUCCEEDED"  then
         local unit = select(2, ...)
         local guid = UnitGUID(unit)
+        local spellId = select(4, ...)
         local newState = aura_env.handleUnitSpellcastSucceeded(...)
         if guid and newState then
-            allstates[guid] = newState
+            allstates[guid..spellId] = newState
             return true
         end
     end
 
     if subEvent == "UNIT_DIED" then
         local sourceGuid = select(9, ...)
-        allstates[sourceGuid] = {changed=true, show=false}
-        aura_env.activeBars[sourceGuid] = nil
+        local pattern = sourceGuid:gsub("%-", "%%-")        for guid_spellId, _ in pairs(allstates) do
+            if string.find(guid_spellId, pattern) then
+                allstates[guid_spellId] = {changed=true, show=false}
+                aura_env.activeBars[guid_spellId] = nil
+            end
+        end
         return true
     end
     
@@ -38,8 +47,8 @@ function(allstates, ...)
     end
 
     if event == "RAID_TARGET_UPDATE" then
-        for guid, state in pairs(allstates) do
-            local unit = UnitTokenFromGUID(guid)
+        for _, state in pairs(allstates) do
+            local unit = UnitTokenFromGUID(state.guid)
             if unit then
                 local mark = GetRaidTargetIndex(unit)
                 state.mark = (mark and ICON_LIST[mark].."16|t") or ""
